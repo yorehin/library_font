b9fde6fbcf4b500f0763578928c607f45aecee16
ylb <253765383@qq.com>
2019-03-11 15:48:34 +0800

    首页基本已经画好

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,6 +1,9 @@
 <template>
 <div>
   <img src="../../assets/img/home/轮播图1.png" height="924" width="1920"/>
+
+
+
   <div id="product">
     <div id="productService"><h1>产品服务</h1></div>
       <div id="productE">
@@ -18,19 +21,84 @@
       </ul>
     </div>
   </div>
-  <div id="new">
 
-    <div id="newProduct0"><h1>最新产品</h1></div>
-    <div id="newProduct1">
-      <div id="newProduct2">
+
+
+  <div id="new">
+    <div class="newProduct0"><h1>最新产品</h1></div>
+    <div class="newProduct1">
+      <div class="newProduct2">
         New Product
       </div>
     </div>
-    <div id="productContext">
+    <div id="newPage">
       <ul>
-        <li><router-link to=""><div class="li-div1"><div class="phone">手机APP设计开发</div><i class="iconfont iconappkaifa"></i><div class="lj1">了解详情</div></div></router-link></li>
-        <li><router-link to=""><div class="li-div1"><div class="phone">小程序设计开发</div><i class="iconfont icon_huabanfuben"></i><div class="lj1">了解详情</div></div></router-link></li>
+        <li><div class="li-div1"><div class="compute"><img src="../../assets/img/home/首页-最新产品-Excel.png" height="300" width="360"/><div class="new-text">Excel小帮手-定制软件</div></div></div></li>
+        <li><div class="li-div1"><div class="compute"><img src="../../assets/img/home/首页-最新产品-汤阴县图书馆.png" height="300" width="360"/><div class="new-text">汤阴县图书馆-WEB网站</div></div></div></li>
+        <li><div class="li-div1"><div class="compute"><img src="../../assets/img/home/首页-最新产品-斑点狗.png" height="300" width="360"/><div class="new-text">斑点狗约课-小程序</div></div></div></li>
       </ul>
+      <div id="div-img"><img src="../../assets/img/home/首页最新产品指示球.png" height="48" width="48"/></div>
+      <div id="div-text"><div>查看更多<i class="iconfont icongengduo" style="color: white;"></i></div></div>
+    </div>
+  </div>
+
+
+  <div id="about">
+    <div class="newProduct0" style="color: black"><h1>关于我们</h1></div>
+    <div class="newProduct1" style="color: black">
+      <div class="newProduct2" style="color: black; border-top: 1px solid black;" >
+        About us
+      </div>
+    </div>
+    <div id="aboutMe">
+      <div id="box0"><div id="box1"><div id="box2"></div></div></div>
+      <i class="iconfont iconfazhan" style="font-size: 183px; color: #0f1937; margin-top: 93px; margin-left: 309px"></i>
+      <div class="about-div-1">
+        <div class="about-circular"><div class="year">2019</div><div class="date">02-15</div></div><div class="a-a">斑点狗约课小程序上线</div>
+      </div>
+      <div class="about-div-2">
+        <div class="about-circular"><div class="year">2018</div><div class="date">11-30</div></div><div class="a-a">第一个项目WEB网站完成</div>
+      </div>
+      <div class="about-div-3">
+        <div class="about-circular"><div class="year">2019</div><div class="date">11-02</div></div><div class="a-a">公司成立</div>
+      </div>
+      <!--<div class="div-line">fdsfd</div>-->
+      <div class="div-right">
+        <div class="div-riq">
+          <div class="div-top">发展历程</div>
+        </div>
+        <div class="div-bottom">企业文化</div>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div id="contact">
+    <div class="newProduct0"><h1>联系我们</h1></div>
+    <div class="newProduct1">
+      <div class="newProduct2">
+        Contact us
+      </div>
+    </div>
+    <div id="contact-top">
+      <div id="contact-left">
+        <div class="font-font"><i class="iconfont iconplace" style="color: white;"></i>&nbsp;&nbsp;地址:河南省安阳市汤阴县信合路南段路东47巷</div>
+        <div class="font-font"><i class="iconfont iconshouji" style="color: white;"></i>&nbsp;&nbsp;电话:155-3723-2100</div>
+        <div class="font-font"><i class="iconfont iconyouxiang" style="color: white;"></i>&nbsp;&nbsp;邮箱:123278392@qq.com</div>
+        <div class="font-font"><i class="iconfont iconweixingongzhonghao" style="color: white;"></i>&nbsp;&nbsp;微信公众号:安阳大玉网络科技有限公司</div>
+        <div class="look-code">扫码关注</div>
+        <div class="use-code"><img src="../../assets/img/home/联系我们-二维码.png" height="220" width="220"/></div>
+      </div>
+      <div class="right-img"><img src="../../assets/img/home/地图.png" height="498" width="700"/></div>a
+      <div id="down"><div class="down-div">&copy;2018  安阳大玉网络科技股份有限公司  版权所有  </div><div class="down-div2"><a id="down-a">豫ICP备17037574号</a></div></div>
+      <!--<ul>-->
+      <!--<li><div class="li-div1"><div class="compute"><img src="../../assets/img/home/首页-最新产品-Excel.png" height="300" width="360"/><div class="new-text">Excel小帮手-定制软件</div></div></div></li>-->
+      <!--<li><div class="li-div1"><div class="compute"><img src="../../assets/img/home/首页-最新产品-汤阴县图书馆.png" height="300" width="360"/><div class="new-text">汤阴县图书馆-WEB网站</div></div></div></li>-->
+      <!--<li><div class="li-div1"><div class="compute"><img src="../../assets/img/home/首页-最新产品-斑点狗.png" height="300" width="360"/><div class="new-text">斑点狗约课-小程序</div></div></div></li>-->
+      <!--</ul>-->
+      <!--<div id="div-img"><img src="../../assets/img/home/首页最新产品指示球.png" height="48" width="48"/></div>-->
+      <!--<div id="div-text"><div>查看更多<i class="iconfont icongengduo"></i></div></div>-->
     </div>
   </div>
 </div>
@@ -119,17 +187,6 @@ a:hover{
   background-color: #fbb03b;
   color: #fff;
 }
-
-
-
-
-
-
-
-
-
-
-
   #new{
     display: inline-block;
     font-size: 30px;
@@ -137,23 +194,266 @@ a:hover{
     width: 1920px;
     background-image: url("../../assets/img/home/首页-最新产品背景图.png");
   }
-#newProduct0{
+.newProduct0{
   color: white;
   margin-top: 175px;/*这个多出来55px本来量的是12px*/
   margin-bottom: 0px;
 }
-#newProduct1{
+.newProduct1{
   margin-top: 10px;
   width: 1920px;
   text-align: center;
 }
-#newProduct2{
+.newProduct2{
   color: white;
   padding: 8px;
   border-top: 1px solid white;
   display: inline;
   text-align: center;
   font-size: 1px;
-  width: 100px;
+  /*width: 500px;*/
 }
+  #newPage ul{
+    height: 400px;
+  }
+  #newPage ul li:first-child{
+    margin-left: 400px;
+    text-align: center;
+  }
+  #newPage ul > :first-child + li{
+    margin-left: 168px;
+  }
+  #newPage ul li:last-child{
+    margin-left: 186px;
+  }
+  .compute{
+    width: 330px;
+    margin-top: 50px;
+    font-size: 23px;
+    text-align: center;
+    margin-bottom: 65px;
+    color: white;
+  }
+  .new-text{
+    margin-top: -45px;
+    margin-left: 30px;
+  }
+  #div-img{
+    text-align:center;
+  }
+  #div-text{
+    margin-top: 80px;
+    text-align: center;
+    font-size: 23px;
+    cursor: pointer;
+    color: white;
+  }
+  #div-text div{
+    margin: 0 auto;
+    width: 200px;
+    text-align: center;
+  }
+.iconfont {
+  -webkit-app-region: no-drag;
+  cursor: pointer;
+  font-size: 23px;
+  margin-left: 10px;
+}
+
+  #about{
+    display: inline-block;
+    font-size: 30px;
+    height: 924px;
+    width: 1920px;
+    background-color: white;
+  }
+  #aboutMe{
+    position:relative;
+    margin: 62px auto 0;
+    width: 1181px;
+    height: 526px;
+  }
+  #box0{
+    width: 70px;
+    height: 70px;
+    border-top: 1px solid black;
+    border-left: 1px solid black;
+  }
+  #box1{
+    margin-top: -1px;
+    margin-left: -1px;
+    width: 20px;
+    height: 20px;
+    border-top: 1px solid white;
+    border-left: 1px solid white;
+  }
+  #box2{
+    margin-top: -2px;
+    margin-left: -2px;
+    width: 10px;
+    height: 10px;
+    background-color: black;
+  }
+  .about-circular{
+    display: inline-block;
+    width: 90px;
+    height: 90px;
+    border-radius: 50px;
+    border: 1px solid #10103b;
+  }
+  .about-div-1{
+    position:absolute;
+    display: inline-block;
+    margin-left: 312px;
+    margin-top: -34px;
+    font-size: 16px;
+    line-height: 92px;
+  }
+  .about-div-2{
+    position:absolute;
+    display: inline-block;
+    margin-left: 123px;
+    margin-top: 147px;
+    font-size: 16px;
+    line-height: 92px;
+  }
+  .about-div-3{
+    position:absolute;
+    display: inline-block;
+    margin-left: -69px;
+    margin-top: 330px;
+    font-size: 16px;
+    line-height: 92px;
+  }
+  .year{
+    margin-top: 25px;
+    font-size: 20px;
+    text-align: center;
+    line-height: 20px;
+  }
+  .date{
+    margin: 0 auto;
+    text-align: center;
+    font-size: 20px;
+    width: 60px;
+    border-top: 1px solid black;
+    line-height: 20px;
+  }
+  .a-a{
+    margin-top: -105px;
+    margin-left: 100px;
+  }
+  .div-line{
+    /*width:100px;*/
+    /*height:100px;*/
+    /*border:1px solid #333;*/
+    background:linear-gradient(45deg,transparent 49.5%,
+    deeppink 49.5%,
+    deeppink 50.5%,
+    transparent 50.5%);
+    /*width:0px;*/
+    /*height:0px;*/
+    /*background-color: red;*/
+    /*border-top: 50px solid white;*/
+    /*border-right: 50px solid green;*/
+    /*border-bottom: 50px solid orange;*/
+    /*border-left: 50px solid white;*/
+  }
+.div-right{
+  position:relative;
+  text-align: center;
+  width: 400px;
+  height: 48px;
+  line-height: 48px;
+  color: black;
+  background-color: #fbb03b;
+  margin-left: 750px;
+  margin-top: 200px;
+}
+/*.div-right a:hover{*/
+  /*color: white;*/
+/*}*/
+.div-riq{
+  width: 200px;
+  display: inline-block;
+  line-height: 48px;
+  margin-left: -70px;
+}
+.div-top{
+  width: 200px;
+  margin-top: 8px;
+  height: 32px;
+
+  line-height: 48px;
+  display: inline-block;
+  border-right: 1px solid black;
+
+}
+.div-bottom{
+  display: inline-block;
+  line-height: 48px;
+  /*padding: 8px;*/
+}
+#contact-top{
+  position:relative;
+}
+#contact{
+  display: inline-block;
+  font-size: 30px;
+  height: 924px;
+  width: 1920px;
+  background-color: #10103b;
+}
+  #contact-left{
+    width: 415px;
+    height: 500px;
+    margin-left: 370px;
+    margin-top: 107px;
+  }
+  .font-font{
+    font-size: 18px;
+    padding: 0 0 22px 0;
+    color: white;
+  }
+  .look-code{
+    text-align: center;
+    color: white;
+    font-size: 24px;
+    margin-top: 50px;
+  }
+  .use-code{
+    text-align: center;
+    margin-top: 30px;
+  }
+  .right-img{
+    position:absolute;
+    margin-top: -500px;
+    margin-left: 800px;
+  }
+  #down{
+    text-align: center;
+    margin-top: 1px;
+    color: white;
+
+  }
+  .down-div{
+    display: inline;
+    margin: 107px 0 0 0;
+    width: 275px;
+    font-size: 12px;
+    border-right: 1px solid white;
+  }
+  .down-div2{
+    display: inline;
+    padding: 4px;
+  }
+  #down-a{
+    margin: 0 auto;
+    display: inline;
+    font-size: 12px;
+    color: white;
+  }
+  #down-a:hover{
+    background-color: #fbb03b;
+  }
 </style>
